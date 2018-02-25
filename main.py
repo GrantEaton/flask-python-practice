6698dd8ee024570c5e7675779d012ee84fb67c88
Sridhar Mocherla <srmocher@email.arizona.edu>
2018-02-24 19:51:04 -0700

    Modified queries to retrieve GeoJSON feature collections for all tables. Added pre and post request methods for opening and closing DB connection respectively.

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from flask import Flask
 from flask import request
-from flask import make_response
+from flask import make_response, Response
 from db import Db
 import pprint
+import json
 
 app = Flask(__name__)
 
 
 if __name__ == "__main__":
-    print "Run using `Flask run` instead."    
+    print "Run using `Flask run` instead."
 
+db = None
+
+@app.before_request
+def open_db():
+    """
+    Open the connection to DB before every request
+    :return:
+    """
+    global db
+    db = Db()
+
+@app.teardown_appcontext
+def close_db(exception):
+    """
+    Close the DB and flush any other resources
+    :param exception: any exception that might have occurred
+    :return:
+    """
+    global db
+    if exception is not None:
+        print exception
+    if db is not None:
+        db.close()
 
 # Queries to be modified to generate FeatureCollection - currently just returns geometry
 
@@ -19,14 +43,18 @@ def map():
     GET method to retrieve map geometry
     :return: GeoJSON FeatureCollection of map geometry
     """
-
-    db = Db()
-    query = "SELECT ST_AsGeoJSON(wkb_geometry) from public.map"
+    global db
+    query = """SELECT row_to_json(fc)
+    FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
+    FROM (SELECT 'Feature' As type
+       , ST_AsGeoJSON(map.wkb_geometry)::json As geometry
+       , row_to_json((SELECT l FROM (SELECT neighborho,street,zip_code,district,accepted,jurisdicti,streetname) As l
+         )) As properties
+      FROM map ) As f )  As fc;"""
     db.query(query)
     print "Returns map data as GeoJSON feature collection"
     rows = db.fetch()
-    db.close()
-    return rows
+    return Response(json.dumps(rows),mimetype="application/json")
 
 
 @app.route("/api/streets")
@@ -35,14 +63,18 @@ def streets():
     GET method to retrieve streets geometry
     :return: GeoJSON FeatureCollection of streets geometry
     """
-
-    query = "SELECT ST_AsGeoJSON(wkb_geometry) from public.streets"
-    db = Db()
+    global db
+    query = """SELECT row_to_json(fc)
+    FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
+    FROM (SELECT 'Feature' As type
+       , ST_AsGeoJSON(st.wkb_geometry)::json As geometry
+       , row_to_json((SELECT l FROM (SELECT street,zip_code,accepted,district,jurisdicti,streetname) As l
+         )) As properties
+      FROM streets as st ) As f )  As fc;"""
     db.query(query)
     print "Returns streets as GeoJSON feature collection"
     rows = db.fetch()
-    db.close()
-    return rows
+    return Response(json.dumps(rows),mimetype="application/json")
 
 # Returns the arteries geometry as a GeoJSON feature collection
 @app.route("/api/arteries")
@@ -51,14 +83,18 @@ def arteries():
     GET method to retrieve arteries geometry
     :return: GeoJSON FeatureCollection of arteries geometry
     """
-
-    query = "SELECT ST_AsGeoJSON(wkb_geometry) from public.arteries"
-    db = Db()
+    global db
+    query = """SELECT row_to_json(fc)
+ FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
+ FROM (SELECT 'Feature' As type
+    , ST_AsGeoJSON(ar.wkb_geometry)::json As geometry
+    , row_to_json((SELECT l FROM (SELECT streetname) As l
+      )) As properties
+   FROM arteries as ar ) As f )  As fc;"""
     db.query(query)
     print "Returns arteries as GeoJSON feature collection"
     rows = db.fetch()
-    db.close()
-    return rows
+    return Response(json.dumps(rows),mimetype="application/json")
 
 
 @app.route("/api/neighborhoods")
@@ -68,14 +104,18 @@ def neighborhoods():
     GET method to retrieve neighborhoods geometry
     :return: GeoJSON FeatureCollection of neighborhoods geometry
     """
-
-    query = "SELECT ST_AsGeoJSON(wkb_geometry) from public.neighborhoods"
-    db = Db()
+    global db
+    query = """SELECT row_to_json(fc)
+    FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
+    FROM (SELECT 'Feature' As type
+       , ST_AsGeoJSON(nhoods.wkb_geometry)::json As geometry
+       , row_to_json((SELECT l FROM (SELECT neighborho) As l
+         )) As properties
+      FROM neighborhoods as nhoods ) As f )  As fc;"""
     db.query(query)
     print "Returns neighborhoods as GeoJSON feature collection"
     rows = db.fetch()
-    db.close()
-    return rows
+    return Response(json.dumps(rows),mimetype="application/json")
 
 
 @app.route("/api/freeways")
@@ -84,14 +124,19 @@ def freeways():
     GET method to retrieve freeways geometry
     :return: GeoJSON FeatureCollection of freeways geometry
     """
-
-    query = "SELECT ST_AsGeoJSON(wkb_geometry) from public.freeways"
-    db = Db()
+    global db
+    query = """SELECT row_to_json(fc)
+ FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features
+ FROM (SELECT 'Feature' As type
+    , ST_AsGeoJSON(fr.wkb_geometry)::json As geometry
+    , row_to_json((SELECT l FROM (SELECT streetname) As l
+      )) As properties
+   FROM freeways as fr ) As f )  As fc;"""
     db.query(query)
     print "Returns freeways as GeoJSON feature collection"
     rows = db.fetch()
-    db.close()
-    return rows
+    return Response(json.dumps(rows),mimetype="application/json")
+
 
 
 
